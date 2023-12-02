438dd9e6cb1f86ffc494d16b3b547a89d705d62f
Robin <seidelr@ethz.ch>
2023-12-02 20:25:35 +0100

    set/fix some basic layout choices

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,83 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import Registration from './components/Registration';
 import Calendar from './components/Calendar';
 import DefaultCalendar from './components/DefaultCalendar';
 
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+  },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
     <Router>
-      <Sidebar />
-      <Routes>
-        <Route path="/register" element={<Registration />} />
-        <Route path="/calendar" element={<Calendar />} />
-        <Route path="/default" element={<DefaultCalendar />} />
-      </Routes>
+      <div className={classes.root}>
+        <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="h6" noWrap>
+              Welcome to the Advent Calendar
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <Toolbar />
+          <List>
+            {/* Add your links here */}
+            <ListItem button>
+              <ListItemText primary="Register" />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="Calendar" />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="Default Calendar" />
+            </ListItem>
+          </List>
+        </Drawer>
+        <main className={classes.content}>
+          <Toolbar />
+          <Routes>
+            <Route path="/register" element={<Registration />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/default" element={<DefaultCalendar />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
