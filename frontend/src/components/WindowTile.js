8332c98f81732cb86918c02ad397db7600579f08
Patrick Beer <pabeer@protonmail.com>
2023-12-03 16:21:53 +0100

    same tile for past and present windows

--- frontend/src/components/WindowTile.js
+++ frontend/src/components/WindowTile.js
@@ -1,11 +1,11 @@
 // WindowTile.js
 import React from 'react';
-import { Card, CardContent, CardMedia, CardActionArea, Typography, Paper, Box } from '@mui/material';
+import { Card, CardMedia, CardActionArea, Typography, Paper} from '@mui/material';
 
 function WindowTile({ window_nr, calendar_id }) {
   // variables to get from SQL request based on window number and calendar id
   const image_path = "https://www.goenhard.ch/wp-content/uploads/P1130972Adventsfenster_Goenhard_Aarau_2020.jpg" // local path?
-  const has_image = true
+  const has_image = false
   const is_free = true
 
   // other variables
@@ -14,28 +14,6 @@ function WindowTile({ window_nr, calendar_id }) {
   const available_visibility = is_free ? {} : { visibility: "hidden" }
   const today_border = is_today ? 2: 0
   const cardSize = 150
-
-  const tileContent = has_image ? (
-    <Box sx={{
-      width: '100%',
-      height: '100%',
-    }}>
-      <CardMedia
-        component="img"
-        image={image_path}
-        alt="advent window"
-        sx={{ width: '100%', height: '100%', objectFit: 'cover'}}
-      />
-      <Paper variant="outlined" sx={{ position: 'absolute', bottom: 5, right: 5, width: '30%', height: '30%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-        <Typography variant="h4">{window_nr}</Typography>
-      </Paper>
-    </Box>
-  ) : (
-    <CardContent>
-      <Typography variant="h2" align="center">{window_nr}</Typography>
-      <Typography variant="body2" align="center" color="text.secondary" sx={available_visibility}>Noch frei</Typography>
-    </CardContent>
-  );
 
   return (
     <Card sx={{
@@ -47,7 +25,48 @@ function WindowTile({ window_nr, calendar_id }) {
         width: '100%',
         height: '100%',
       }}>
-        {tileContent}
+        <CardMedia
+          component="img"
+          image={has_image ? image_path : placeholder_path}
+          alt="advent window"
+          sx={{ width: '100%', height: '100%', objectFit: 'cover'}}
+        />
+        <Paper
+          variant="outlined"
+          sx={{
+            position: 'absolute',
+            bottom: 5,
+            left: 5,
+            paddingLeft: 1,
+            paddingRight: 1,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+        >
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            sx={available_visibility}
+          >
+            Noch frei
+          </Typography>
+        </Paper>
+        <Paper
+          variant="outlined"
+          sx={{
+            position: 'absolute',
+            bottom: 5,
+            right: 5,
+            width: '32%',
+            height: '32%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+        >
+          <Typography variant="h4">{window_nr}</Typography>
+        </Paper>
       </CardActionArea>
     </Card>  
   );
