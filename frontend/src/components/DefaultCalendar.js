7119583f9f30074022f72c2ccf55d7219ebb61bf
Patrick Beer <pabeer@protonmail.com>
2023-12-03 16:03:09 +0100

    Image cropped to fill card

--- frontend/src/components/DefaultCalendar.js
+++ frontend/src/components/DefaultCalendar.js
@@ -6,17 +6,18 @@ import WindowTile from './WindowTile';
 function DefaultCalendar({ name, details }) {
   // variables to get from SQL request
   const calendar_id = 0 // pass it to windowTiles and send 24 requests or get all info in this component?
-
+  
+  // other variables
   const window_nrs = Array(24).fill().map((_, index) => index + 1);
 
   return (
     <div>
-      <Typography variant="h2">{name}</Typography>
+      <Typography variant="h1" align="center">{name}</Typography>
       <Typography variant="body1">{details}</Typography>
       <Typography variant="body1">Hello from DefaultCalendar.js</Typography>
       <Grid container spacing={2}>
         {window_nrs.map((window_nr) =>
-          <Grid item>
+          <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
             <WindowTile window_nr={window_nr} calendar_id={calendar_id} />
           </Grid>
         )}
--- frontend/src/components/WindowTile.js
+++ frontend/src/components/WindowTile.js
@@ -1,37 +1,52 @@
 // WindowTile.js
 import React from 'react';
-import { Card, CardContent, CardMedia, CardActionArea, Typography } from '@mui/material';
+import { Card, CardContent, CardMedia, CardActionArea, Typography, Paper, Box } from '@mui/material';
 
 function WindowTile({ window_nr, calendar_id }) {
   // variables to get from SQL request based on window number and calendar id
   const image_path = "https://www.goenhard.ch/wp-content/uploads/P1130972Adventsfenster_Goenhard_Aarau_2020.jpg" // local path?
-  const has_image = false
+  const has_image = true
   const is_free = true
 
   // other variables
   const is_today = new Date().getDate() === window_nr
+  const placeholder_path = "https://clipground.com/images/christmas-door-clipart-1.jpg"
   const available_visibility = is_free ? {} : { visibility: "hidden" }
+  const today_border = is_today ? 2: 0
+  const cardSize = 150
 
   const tileContent = has_image ? (
-    <div>
+    <Box sx={{
+      width: '100%',
+      height: '100%',
+    }}>
       <CardMedia
         component="img"
-        height="150"
         image={image_path}
         alt="advent window"
+        sx={{ width: '100%', height: '100%', objectFit: 'cover'}}
       />
-      <Typography variant="h4">{window_nr}</Typography>
-    </div>
+      <Paper variant="outlined" sx={{ position: 'absolute', bottom: 5, right: 5, width: '30%', height: '30%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+        <Typography variant="h4">{window_nr}</Typography>
+      </Paper>
+    </Box>
   ) : (
     <CardContent>
-      <Typography variant="h2">{window_nr}</Typography>
-      <Typography variant="body2" color="text.secondary" style={available_visibility}>Noch frei</Typography>
+      <Typography variant="h2" align="center">{window_nr}</Typography>
+      <Typography variant="body2" align="center" color="text.secondary" sx={available_visibility}>Noch frei</Typography>
     </CardContent>
   );
 
   return (
-    <Card>
-      <CardActionArea>
+    <Card sx={{
+      width: cardSize,
+      height: cardSize,
+      border: today_border,
+    }}>
+      <CardActionArea sx={{
+        width: '100%',
+        height: '100%',
+      }}>
         {tileContent}
       </CardActionArea>
     </Card>  
