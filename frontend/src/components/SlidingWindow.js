e2c086a1cb50cdae82541075a2bafeb4f77a0609
Leon Fröhlich <lfroehlich@student.ethz.ch>
2023-12-05 21:16:15 +0100

    Styling Windows

--- frontend/src/components/SlidingWindow.js
+++ frontend/src/components/SlidingWindow.js
@@ -13,7 +13,7 @@ function SlidingWindow({window_nr, calendar_id, onClose, windows_coordinates}) {
     let has_apero = true;
     let start_time = "18.00";
     let end_time = "20.00";
-    let address = "some address";
+    let address = "Stockerstrasse 23, 8050 Zürich";
     const pers_comments = [["Person1", "Hey, it was great"], ["Person2", "Thanks so much Misses Seidel"], ["Person3", "I liked the Apéro"], ["Person4", "See you next year"]];
 
 
@@ -37,57 +37,60 @@ function SlidingWindow({window_nr, calendar_id, onClose, windows_coordinates}) {
 
     return(
         <Dialog open={true} onClose={onClose} sx={{ zIndex: 9999, position: 'absolute' }}>
-            <DialogContent sx={{ width: '500px', height: '400px' }}>
-            <Typography variant="h4">Calendar ID: {calendar_id}</Typography>
-            <Typography variant="h5">Window Number: {window_nr}</Typography>
-            <Typography variant="h5">{has_apero ? "Mit Apero" : "Ohne Apero"}</Typography>
-    
+            <DialogContent sx={{ width: '400px', height: '700px' }}>
 
+                <Typography variant="h4">Window #{window_nr}</Typography>
+                <Typography variant="body1">{address}</Typography>
+                <Typography variant="body1" style={{color: 'blue'}}>{start_time + " - " + end_time}</Typography>
 
-            <MapContainer center={windows_coordinates[0]} zoom={13} scrollWheelZoom={false} style={{ height: "300px", width: "80%", margin: 20}}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                <MapContainer center={windows_coordinates[0]} zoom={13} scrollWheelZoom={false} style={{ height: "300px", width: "80%", margin: 20}}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                        {windows_coordinates.map((window_coordinates, index) => 
+                            <Marker position={window_coordinates} icon={new L.icon({ iconUrl: icon_path, iconSize: [32, 32]})}>
+                                <Popup>
+                                    <div>{window_nr}. Dezember<br /></div>
+                                    <div>Address: {address}</div>
+                                    <div>Time: {start_time + " - " + end_time}</div>
+                                    <div>Location hint: {location_hint}</div>
+                                </Popup>
+                            </Marker>
+                        )}
+                </MapContainer>
+
+                <Typography variant="body1">Apéro: {has_apero ? "Ja" : "Nein"}</Typography>
+                <Typography variant='body1'>Location Hint: {location_hint}</Typography>
+
+                {/* Comment section */}
+                <List sx={{ marginTop: 2, padding: 2, border: '1px solid #ccc', borderRadius: 4 }}>
+                    {pers_comments.map((pers_comment, index) => (
+                        <ListItem key={index} alignItems="flex-start">
+                        <span><ListItemText primary={pers_comment[0]} /><ListItemText primary={pers_comment[1]} /></span>
+                        </ListItem>
+                    ))}
+                </List>
+
+                {/* Form for adding a new comment */}
+                <TextField
+                label="Add your comment"
+                variant="outlined"
+                fullWidth
+                value={newComment}
+                onChange={handleCommentChange}
                 />
-                {windows_coordinates.map((window_coordinates, index) => 
-                    <Marker position={window_coordinates} icon={new L.icon({ iconUrl: icon_path, iconSize: [32, 32]})}>
-                    <Popup>
-                        <div>{window_nr}. Dezember<br /></div>
-                        <div>Address: {address}</div>
-                        <div>Time: {start_time + " - " + end_time}</div>
-                        <div>Location hint: {location_hint}</div>
-                    </Popup>
-                    </Marker>
-                )}
-            </MapContainer>
 
-            {/* Comment section */}
-            <List sx={{ marginTop: 2, padding: 2, border: '1px solid #ccc', borderRadius: 4 }}>
-            {pers_comments.map((pers_comment, index) => (
-                <ListItem key={index} alignItems="flex-start">
-                <span><ListItemText primary={pers_comment[0]} /><ListItemText primary={pers_comment[1]} /></span>
-                </ListItem>
-            ))}
-            </List>
+                <Button variant="contained" color="primary" onClick={handleAddComment}>
+                    Add Comment
+                </Button>
+                <br/>
 
-            {/* Form for adding a new comment */}
-            <TextField
-            label="Add your comment"
-            variant="outlined"
-            fullWidth
-            value={newComment}
-            onChange={handleCommentChange}
-            />
-
-           <div><Button variant="contained" color="primary" onClick={handleAddComment}>
-            Add Comment
-            </Button></div>
-
-            { /* add more content here */ }
-            <Button variant="contained" color="primary" onClick={onClose}>
-              Close
-            </Button>
-          </DialogContent>
+                { /* add more content here */ }
+                <Button variant="contained" color="primary" onClick={onClose}>
+                    Close
+                </Button>
+            </DialogContent>
         </Dialog>
       );
 }
