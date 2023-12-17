b1729e8a2c1481ca6b03f94616a16a0f71cf12d1
Patrick Beer <pabeer@protonmail.com>
2023-12-17 15:32:16 +0100

    Center map based on marker average

--- frontend/src/components/DrawMap.js
+++ frontend/src/components/DrawMap.js
@@ -5,12 +5,9 @@ import React, { useCallback, useState, useEffect  } from 'react';
 
 
 const DrawMap = ({ coordinatesList, iconPath, drawNumbers }) => {
-  const [coordinates, setCoordinates] = useState([]);
-  const [centerCoordinates, setCenterCoordinates] = useState([0, 0]);
-  //console.log("DrawMap coordinates:", coordinates);
-  //console.log("DrawMap centerCoordinates:", centerCoordinates)
-  useEffect(() => {
-    setCoordinates(coordinatesList);
+  const [centerCoordinates, setCenterCoordinates] = useState([]);
+
+  const calculateCenter = () => {
     let sumLatitude = 0;
     let sumLongitude = 0;
     const nr_markers = coordinatesList.length;
@@ -21,25 +18,36 @@ const DrawMap = ({ coordinatesList, iconPath, drawNumbers }) => {
       }
       setCenterCoordinates([sumLatitude / nr_markers, sumLongitude / nr_markers]);
     }
+  }
+
+  useEffect(() => {
+    calculateCenter();
   }, [coordinatesList]);
 
-  return (
-    <MapContainer center={centerCoordinates} zoom={13} scrollWheelZoom={false} style={{ height: "300px", width: "80%", margin: 20 }}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {coordinates.length == 0 ? <></> : coordinates.map((point) => (
-        <Marker position={[point.x, point.y]} icon={new L.icon({ iconUrl: iconPath, iconSize: [32, 32] })}>
-          <Popup>
-            {/* {drawNumbers && <div>{index}. Dezember<br /></div>} */}
-            {/* Need to get index from window nr */}
-            {/* Add additional details here if needed */}
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
-  );
+
+  if (centerCoordinates.length > 0) {
+    return (
+      <MapContainer center={centerCoordinates} zoom={13} scrollWheelZoom={false} style={{ height: "300px", width: "80%", margin: 20 }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {coordinatesList.length == 0 ? <></> : coordinatesList.map((point, index) => (
+          <Marker key={index} position={[point.x, point.y]} icon={new L.icon({ iconUrl: iconPath, iconSize: [32, 32] })}>
+            <Popup>
+              {/* {drawNumbers && <div>{index}. Dezember<br /></div>} */}
+              {/* Need to get index from window nr */}
+              {/* Add additional details here if needed */}
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    );
+  } else {
+    return (
+      <></>
+    );
+  }
 };
 
 export default DrawMap;
