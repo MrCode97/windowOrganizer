5ddc6dd55ddd750bfd1cba93f2b7cf2caccc977d
Robin <seidelr@ethz.ch>
2023-12-19 02:29:35 +0100

    only if loggedIn show CalendarRegistartion

--- frontend/src/components/AdventCalendarRegistrationForm.js
+++ frontend/src/components/AdventCalendarRegistrationForm.js
@@ -1,12 +1,38 @@
 // AdventCalendarRegistrationForm.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Typography, TextField, Button, Snackbar} from '@mui/material';
+
+function LoginHint() {
+  return (
+    <div>
+      <Typography variant="h4">Advent Calendar Registration</Typography>
+      <Typography variant="p">Please log in first to register an advent calendar.</Typography>
+    </div>
+  );
+}
 
 function AdventCalendarRegistrationForm( { reRender } ) {
   const [adventCalendarId, setAdventCalendarId] = useState('');
   const [username, setUsername] = useState('');
   const [message, setMessage] = useState('');
   const [messageOpen, setMessageOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+    useEffect(() => {
+      // Check if the user is authenticated when the component mounts
+      const checkAuthentication = () => {
+        const token = localStorage.getItem('token');
+        if (token) {
+          // User is logged in
+          setIsLoggedIn(true);
+        } else {
+          // User is not logged in
+          setIsLoggedIn(false);
+        }
+      };
+  
+      checkAuthentication();
+    }, []);  
 
   // API request
   const handleSubmit = async (event) => {
@@ -18,6 +44,7 @@ function AdventCalendarRegistrationForm( { reRender } ) {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + localStorage.getItem('token'),
         },
         body: JSON.stringify({ adventCalendarId, username }),
       });
@@ -48,25 +75,28 @@ function AdventCalendarRegistrationForm( { reRender } ) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Typography className='registrationHeader' variant="h4">Advent Calendar Registration</Typography>
-      <TextField
-        label="Your username"
-        fullWidth
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <TextField
-        label="Advent Calendar Name"
-        fullWidth
-        value={adventCalendarId}
-        onChange={(e) => setAdventCalendarId(e.target.value)}
-      />
-      <Button type="submit" variant="contained" sx={{backgroundColor: 'green', marginTop: '10px'}}>
-        Register
-      </Button>
-      <Snackbar open={messageOpen} autoHideDuration={3000} onClose={handleClose} message={message} />
-    </form>
+    <div> {isLoggedIn ? (
+      <form onSubmit={handleSubmit}>
+        <Typography className='registrationHeader' variant="h4">Advent Calendar Registration</Typography>
+        <TextField
+          label="Your username"
+          fullWidth
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <TextField
+          label="Advent Calendar Name"
+          fullWidth
+          value={adventCalendarId}
+          onChange={(e) => setAdventCalendarId(e.target.value)}
+        />
+        <Button type="submit" variant="contained" sx={{backgroundColor: 'green', marginTop: '10px'}}>
+          Register
+        </Button>
+        <Snackbar open={messageOpen} autoHideDuration={3000} onClose={handleClose} message={message} />
+      </form>)
+      : (<LoginHint />)}
+    </div>
   );
 }
 
